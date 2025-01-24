e43639e91fca120318cee6f47a13f4df72d50bcd
Claudia Villarroel <claudia.villarroel@live.com>
2025-01-24 18:07:47 -0300

    :hammer: Ajustes en retornos

--- Entrega1/src/routes/products.router.js
+++ Entrega1/src/routes/products.router.js
@@ -3,103 +3,171 @@ const router = Router();
 const { uploader } = require('../utils/utils.js');
 const fs = require('fs');
 const path = require('path');
-const { products } = require('../utils/data/products.json');
+const rutaProducts = path.join(__dirname, '../utils/data/products.json');
+const Product = require('../models/product.js');
 
 //GET /api/products
-// Listar productos
 router.get('/', (req, res) => {
-  const limit = parseInt(req.query.limit) || products.length;
-  const limitedProducts = products.slice(0, limit);
-  res.send(limitedProducts);
-});
-
-//GET /api/products/:pid
-// Obtener un producto por su ID
-router.get('/:pid', (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const product = products.find((p) => p.id === productId);
-  if (!product) {
-    res.status(404).send({ error: 'Producto no encontrado' });
-  } else {
-    res.send(product);
+  try {
+    let productsData = JSON.parse(fs.readFileSync(rutaProducts, 'utf-8'));
+    const limit = parseInt(req.query.limit) || productsData.length;
+    const limitedProducts = productsData.slice(0, limit);
+    res.status(200).send({
+      status: 'success',
+      data: limitedProducts,
+      message: 'Productos obtenidos correctamente',
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: 'error',
+      data: [],
+      message: 'Error al obtener los productos. Detalles: ' + error.message,
+    });
   }
 });
 
-//POST /api/products
-// Crear un nuevo producto
-router.post('/', (req, res) => {
+//GET /api/products/:pid
+router.get('/:pid', (req, res) => {
   try {
-    const newProduct = req.body;
-    if (
-      !newProduct.title ||
-      !newProduct.description ||
-      !newProduct.price ||
-      !newProduct.code ||
-      !newProduct.stock
-    ) {
-      res.status(400).send({ error: 'Faltan datos requeridos' });
+    const productId = parseInt(req.params.pid);
+    let productsData = JSON.parse(fs.readFileSync(rutaProducts, 'utf-8'));
+    const product = productsData.find((p) => p.id === productId);
+    if (!product) {
+      res
+        .status(404)
+        .send({ status: 'error', data: [], message: 'Producto no encontrado' });
     } else {
-      let productsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../utils/data/products.json'), 'utf-8'));
-      const newProductId = productsData.length ? productsData[productsData.length - 1].id + 1 : 1;
-
-      newProduct.id = newProductId;
-      productsData.push(newProduct);
-      console.log(productsData);
-      fs.writeFileSync(
-        path.join(__dirname, '../utils/data/products.json'),
-        JSON.stringify(productsData, null, 2)
-      );
-      res.send(newProduct);
+      res.status(200).send({
+        status: 'success',
+        data: product,
+        message: 'Producto obtenido correctamente',
+      });
     }
   } catch (error) {
-    res.status(500).send({ error: 'Error al crear el producto. Detalles: ' + error.message });
+    res.status(500).send({
+      status: 'error',
+      data: [],
+      message: 'Error al obtener el producto. Detalles: ' + error.message,
+    });
+  }
+});
+
+// POST /api/products
+router.post('/', (req, res) => {
+  try {
+    const data = req.body;
+    if (
+      !data.title ||
+      !data.description ||
+      !data.price ||
+      !data.code ||
+      !data.stock ||
+      !data.category
+    ) {
+      res.status(400).send({
+        status: 'error',
+        data: [],
+        message: 'Faltan datos requeridos',
+      });
+    } else {
+      let productsData = JSON.parse(fs.readFileSync(rutaProducts, 'utf-8'));
+      let newProduct = new Product(
+        productsData.length + 1,
+        data.title,
+        data.description,
+        data.code,
+        data.price,
+        data.stock,
+        data.category,
+        data.thumbnail
+      );
+      productsData.push(newProduct);
+      fs.writeFileSync(rutaProducts, JSON.stringify(productsData, null, 2));
+      res.status(200).send({
+        status: 'success',
+        data: newProduct,
+        message: 'Producto creado correctamente',
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      status: 'error',
+      data: [],
+      message: 'Error al crear el producto. Detalles: ' + error.message,
+    });
   }
 });
 
 //PUT /api/products/:pid
-// Actualizar un producto existente
 router.put('/:pid', (req, res) => {
   try {
-    console.log(req.body);
     const productId = parseInt(req.params.pid);
-    const updatedProduct = req.body;
-    let productsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../utils/data/products.json'), 'utf-8'));
-    console.log(productsData);
+    const data = req.body;
+    let productsData = JSON.parse(
+      fs.readFileSync(rutaProducts, 'utf-8')
+    );
     const productIndex = productsData.findIndex((p) => p.id === productId);
     if (productIndex === -1) {
-      res.status(404).send({ error: 'Producto no encontrado' });
+      res
+        .status(404)
+        .send({ status: 'error', data: [], message: 'Producto no encontrado' });
     } else {
-      productsData[productIndex] = { ...productsData[productIndex], ...updatedProduct };
-      fs.writeFileSync(
-        path.join(__dirname, '../utils/data/products.json'),
-        JSON.stringify(productsData, null, 2)
-      );
-      res.send(updatedProduct);
+      productsData[productIndex] = {
+        ...productsData[productIndex],
+        ...new Product(
+          productId,
+          data.title || productsData[productIndex].title,
+          data.description || productsData[productIndex].description,
+          data.code || productsData[productIndex].code,
+          data.price || productsData[productIndex].price,
+          data.stock || productsData[productIndex].stock,
+          data.category || productsData[productIndex].category,
+          data.thumbnail || productsData[productIndex].thumbnail
+        ),
+      };
+      fs.writeFileSync(rutaProducts, JSON.stringify(productsData, null, 2));
+      res.status(200).send({
+        status: 'success',
+        data: productsData[productIndex],
+        message: 'Producto actualizado correctamente',
+      });
     }
   } catch (error) {
-    res.status(500).send({ error: 'Error al actualizar el producto. Detalles: ' + error.message });
+    res.status(500).send({
+      status: 'error',
+      data: [],
+      message: 'Error al actualizar el producto. Detalles: ' + error.message,
+    });
   }
 });
 
 //DELETE /api/products/:pid
-// Eliminar un producto existente
 router.delete('/:pid', (req, res) => {
   try {
-    const productId = parseInt(req.params.pid); 
-    let productsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../utils/data/products.json'), 'utf-8'));
+    const productId = parseInt(req.params.pid);
+    let productsData = JSON.parse(
+      fs.readFileSync(rutaProducts, 'utf-8')
+    );
     const productIndex = productsData.findIndex((p) => p.id === productId);
     if (productIndex === -1) {
-    res.status(404).send({ error: 'Producto no encontrado' });
-  } else {
-    productsData.splice(productIndex, 1);
-    fs.writeFileSync(
-      path.join(__dirname, '../utils/data/products.json'),
-      JSON.stringify(productsData, null, 2)
-    );
-      res.send({ message: 'Producto eliminado correctamente' });
+      res
+        .status(404)
+        .send({ status: 'error', data: [], message: 'Producto no encontrado' });
+    } else {
+      productsData.splice(productIndex, 1);
+      fs.writeFileSync(rutaProducts, JSON.stringify(productsData, null, 2));
+      res.status(200).send({
+        status: 'success',
+        data: [],
+        message: 'Producto eliminado correctamente',
+      });
     }
   } catch (error) {
-    res.status(500).send({ error: 'Error al eliminar el producto. Detalles: ' + error.message });
+    res.status(500).send({
+      status: 'error',
+      data: [],
+      message: 'Error al eliminar el producto. Detalles: ' + error.message,
+    });
   }
 });
 
