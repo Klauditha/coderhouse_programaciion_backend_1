5071563fd7326825350524e54891004df136e932
Claudia Villarroel <claudia.villarroel@live.com>
2025-03-03 22:27:47 -0300

    :construction: Eliminar producto del carrito en proceso

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -50,21 +50,23 @@ router.post('/:cid/product/:pid', async (req, res) => {
     let idCart = req.params.cid;
     let idProduct = req.params.pid;
     let quantityReq = req.body.quantity;
+    /* Busco el carrito y el producto */
     let cart = await cartModel.findById(idCart);
     let product = await productModel.findById(idProduct);
     let agregado = false;
-    let cartUpdate = null;
+    /* Si el carrito y el producto existen */
     if (cart && product) {
       let idProduct = product._id;
       cart.products.forEach(async (p) => {
         let idProductCart = p._id;
+        /* Si el producto existe en el carrito */
         if (idProductCart.equals(idProduct)) {
           agregado = true;
           p.quantity += quantityReq;
           await cartModel.findByIdAndUpdate(idCart, {
             $set: { products: cart.products },
           });
-          cartUpdate = await cartModel.findById(idCart);
+          let cartUpdate = await cartModel.findById(idCart);
           res.status(200).send({
             status: 'success',
             data: cartUpdate,
@@ -96,6 +98,55 @@ router.post('/:cid/product/:pid', async (req, res) => {
       data: [],
       message:
         'Error al agregar el producto al carrito. Detalles: ' + error.message,
+    });
+  }
+});
+
+//Eliminar producto del carrito
+router.delete('/:cid/products/:pid', async (req, res) => {
+  try {
+    let idCart = req.params.cid;
+    let idProduct = req.params.pid;
+    let cart = await cartModel.findById(idCart);
+    let product = await productModel.findById(idProduct);
+    if (cart && product) {
+      cart.products.forEach(async (p) => {
+        let idProductCart = p._id;
+        if (idProductCart.equals(idProduct)) {
+          await cartModel.findByIdAndUpdate(idCart, {
+            $pull: { products: { _id: product._id } },
+          });
+          let cartUpdate = await cartModel.findById(idCart);
+          console.log(cartUpdate);
+          res.status(200).send({
+          status: 'success',
+          data: cartUpdate,
+            message: 'Producto eliminado correctamente',
+          });
+        }
+      });      
+    } else {
+      console.log(cart);
+      console.log(product);
+      if (!cart) {
+        res.status(404).send({
+          status: 'error',
+          data: [],
+          message: 'Carrito no encontrado',
+        });
+      } else {
+        res.status(404).send({
+          status: 'error',
+          data: [],
+          message: 'Producto no encontrado',
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).send({
+      status: 'error',
+      data: [],
+      message: 'Error al eliminar el producto del carrito. Detalles: ' + error.message,
     });
   }
 });
